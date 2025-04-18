8a48d56825c6e0b8efcffe9d142dcaff75cb4bd1
Daniel P.K <danilofletcher978@gmail.com>
2025-04-18 15:50:03 +0000

    functions done

--- mortal_kombat.py
+++ mortal_kombat.py
@@ -1,3 +1,15 @@
 import pygame 
 
 
+class Fighter:
+    def __init__(self):
+        pass
+    
+    def move():
+        pass 
+    
+    def draw():
+        pass 
+    
+    def update():
+        pass
